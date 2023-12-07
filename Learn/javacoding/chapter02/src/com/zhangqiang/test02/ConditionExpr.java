c01014749bbb8ff8b8f8850133901407c82a4858
zhangqiangg <18742519725@163.com>
2023-12-07 14:43:31 +0800

    new file:   Learn/javacode/.idea/compiler.xml

---
+++ Learn/javacoding/chapter02/src/com/zhangqiang/test02/ConditionExpr.java
@@ -0,0 +1,14 @@
+/*
+ 今天是周2，10天以后是周几？
+
+ 要求：控制台输出“今天是周2，10天以后是周X”。
+*/
+
+public class ConditionExpr {
+    public static void main(String[] args) {
+        int week = 4;
+        week += 10;
+        week %=7;
+        System.out.println("今天是周2,10天之后是周"+((week==0)?("日"):week));
+    }
+}
